b11d4ee31e61e4a4c29ec97364cbce638b72709d
Lucas Israel <lisraelalves92@gmail.com>
2022-06-29 17:23:54 -0300

    demorei mais do que deveria

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
+
+const getSpeciesByIds = (...ids) => (ids === undefined ? []
+  : ids.map((R) => species.find((E) => E.id === R)));
+
+console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
