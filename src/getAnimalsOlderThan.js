555c7f73d265bb8189264023aa5bbdec5b81674f
Lucas Israel <lisraelalves92@gmail.com>
2022-06-29 17:39:15 -0300

    2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
+
+const getAnimalsOlderThan = (animal, age) => species.some((E) =>
+  E.name === animal && E.residents.every((R) => R.age >= age));
+
+// console.log(getAnimalsOlderThan('lions', 50));
 
 module.exports = getAnimalsOlderThan;
