fb300cae13f6d8e4cfad815c989a43c07226e68e
Lucas Israel <lisraelalves92@gmail.com>
2022-06-29 17:51:23 -0300

    3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const { employees } = data;
+
+const getEmployeeByName = (employeeName) => (employeeName === undefined ? {} : employees.find((E) =>
+  E.firstName === employeeName || E.lastName === employeeName));
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
