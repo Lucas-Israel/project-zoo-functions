7f418941bd6bc7adcd098de800686b58f3fca2b4
Lucas Israel <lisraelalves92@gmail.com>
2022-06-30 19:50:44 -0300

    9 nao feito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species, employees } = data;
+
+const getAnimalFromEmployee = (id) => employees.find((E) => E.id === id)
+  .responsibleFor.find((E) => E);
+
+const oldestAnimalfromId = (id) => Math.max(...species.find((E) => E.id === id)
+  .residents.map((E) => E.age));
+
+const getOldestFromFirstSpecies = (id) => Object.values(species.find((E) =>
+  E.id === getAnimalFromEmployee(id)).residents.find((E) =>
+  E.age === oldestAnimalfromId(getAnimalFromEmployee(id))));
+
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { species } = data;
+
+// const resultado = {};
+
+const oqDisponivel = (A) => {
+  let abc = 0;
+  abc += 1;
+  return abc;
+};
+
+const getSchedule = (A) => {
+  let abc = 0;
+  if (A === undefined) ;
+  abc = species.find((E) => A === E.name).availability;
+  return abc;
+};
+
+// console.log(getSchedule('lions'));
+
+console.log(oqDisponivel('Wednesday'));
 
 module.exports = getSchedule;
+
+// const chave = 'monday';
+// const test = {};
+// test[hours] = 'funcionou';
+
+// console.log(test.monday);
