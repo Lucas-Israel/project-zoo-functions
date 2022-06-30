26848bc199b5d6199e7b98c50f425af5315569e5
Lucas Israel <lisraelalves92@gmail.com>
2022-06-30 16:02:30 -0300

    removendo lixo do requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,15 +2,6 @@ const data = require('../data/zoo_data');
 
 const { prices } = data;
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 const countEntrants = (A) => ({ child: A.filter((E) => E.age < 18 && E.age > 0).length,
   adult: A.filter((E) => E.age > 17 && E.age < 50).length,
   senior: A.filter((E) => E.age > 49).length });
