c0a51d44af839eb861f813f0b2d1abd0c520f313
Lucas Israel <lisraelalves92@gmail.com>
2022-06-30 13:20:02 -0300

    5 e 6 feitos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const listando = () => species.reduce((a, b) => ({ ...a, [b.name]: b.residents.length }), 0);
+
+const filtrado = (animal) => (animal.sex === undefined
+  ? species.find((E) => E.name === animal.specie).residents
+  : species.find((E) => E.name === animal.specie).residents
+    .filter((E) => E.sex === animal.sex)).length;
+
+const countAnimals = (animal) => (animal === undefined ? listando() : filtrado(animal));
+
+// console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,32 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Se ao receber "count" como parametro retorna o valor esperado', () => {
+    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('count')).not.toEqual(5);
+  });
+  it('Se ao receber "names" como parametro retorna um array de nomes com os valores esperados', () => {
+    expect(handlerElephants('names')).toContain('Ilana');
+    expect(handlerElephants('names')).toContain('Orval');
+    expect(handlerElephants('names')).toContain('Bea');
+    expect(handlerElephants('names')).toContain('Jefferson');
+    expect(handlerElephants('names')).not.toContain(5);
+  });
+  it('Se ao receber "averageAge" como parametro retorna o valor esperado', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    expect(handlerElephants('averageAge')).not.toEqual(15);
+  });
+  it('Se ao não receber um parametro retorna nada', () => {
+    expect(handlerElephants()).toBe();
+  });
+  it('Se ao escrever um parametro que não esteja previsto retorna null', () => {
+    expect(handlerElephants('abcd')).toBeNull();
+  });
+  it('Se o parametro for diferente de uma string retorna uma string de aviso', () => {
+    expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Se o parametro passado existir como uma chave no objeto, retorna o valor correto', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('location')).toBe('NW');
+  });
+});
